59b5ef147a1ce7056465fa753c31aaeef174063c
Jay Khan <jaykhan.sound@gmail.com>
2021-12-07 02:08:47 -0500

    added vue

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'todos',
     'apis',
     'rest_framework',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -41,11 +42,17 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8001",
+    "http://localhost:8001",
 ]
 
 ROOT_URLCONF = 'config.urls'
